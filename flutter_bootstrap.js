b4a23b4db51e4d786b41e19d581ffdb3ef56ec89
dinesh9908 <155910649+dinesh9908@users.noreply.github.com>
2025-01-06 10:46:49 +0530

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "646375615"
+    serviceWorkerVersion: "1576936621"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "6e195e266b65dcacacf71f4372dcc57b
 "compass.js": "c45e5b29a48562548492477382d73314",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "0af14f942bfe29dd2e34f1ba19a4cb26",
+"flutter_bootstrap.js": "4bda52b279b3411659c7f9a3f69a5226",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
